84fb8c1bf1d62ed48003ff4414a63abb9b2cf476
Xavi <xavifuefer@gmail.com>
2016-08-30 00:02:12 +0100

    fix sort and filtering combined

--- client/src/grid.js
+++ client/src/grid.js
@@ -6,9 +6,9 @@ class Grid {
     this.data = data;
     // todo: use localstorage to persist the state
     this.state = {
-      data: data,
-      orderBy: 'title',
-      filterBy: null
+      data: Object.assign([], data),
+      orderBy: '',
+      filterBy: ''
     };
   }
 
@@ -31,7 +31,7 @@ class Grid {
     this.$el.appendChild(frag);
 
     // update the data inside the grid
-    this.updateGrid(this.state.data);
+    this.updateGrid();
   }
 
   buildToolbar() {
@@ -56,10 +56,24 @@ class Grid {
     $header.classList.add('header');
 
     $header.innerHTML = `
-    <article class="cell title">Title</article>
-    <article class="cell price">Price</article>
-    <article class="cell stars">Stars</article>
+      <article class="cell title">
+        <span>Title</span>
+        <section class="actions">
+          <button data-action="sort-title-asc">&#x25B2;</button>
+          <button data-action="sort-title-des">&#x25BC;</button>
+        </section>
+      </article>
+      <article class="cell price">
+        <span>Price</span>
+        <section class="actions">
+          <button data-action="sort-price-asc">&#x25B2;</button>
+          <button data-action="sort-price-des">&#x25BC;</button>
+        </section>
+      </article>
+      <article class="cell stars"><span>Stars</span></article>
     `;
+
+    $header.addEventListener('click', this.handleClick.bind(this));
 
     return $header;
   }
@@ -76,35 +90,84 @@ class Grid {
     }
 
     $row.innerHTML = `
-    <article class="cell title">${rowData.title || ''}</article>
-    <article class="cell price">${rowData.price || ''}</article>
-    <article class="cell stars">${rowData.stars || 0}</article>
+      <article class="cell title">${rowData.title || ''}</article>
+      <article class="cell price">${rowData.price || ''}</article>
+      <article class="cell stars">${rowData.stars || 0}</article>
     `;
 
     return $row;
   }
 
-  updateGrid(data) {
+  updateGrid() {
     const grid = this.$el.querySelector('.grid-data');
+    const newState = Object.assign({}, this.state);
 
     grid.innerHTML = '';
 
-    data.forEach(row => {
+    newState.data = newState.filterBy ? this.filterGrid(newState.data, newState.filterBy) : this.data;
+    newState.data = this.sortGrid(newState.data, newState.sortBy);
+
+    newState.data.forEach(row => {
       grid.appendChild(this.buildRow(row));
     });
+
+    this.state = newState;
+
+    console.log('state -> ', newState);
   }
 
-  handleKeyup(e) {
-    let data;
+  // sort the grid
+  sortGrid(data, action) {
+    // let data = Object.assign([], this.state.data);
 
-    if (e.target.val === '') data = this.data;
-    else {
-      data = this.state.data.filter(r => {
-        return (new RegExp(e.target.value.toLowerCase())).test(r.title.toLowerCase()) && r;
+    switch (action) {
+      case 'sort-title-asc': {
+        data.sort((a, b) => a.title < b.title ? -1 : 1);
+        break;
+      }
+
+      case 'sort-title-des': {
+        data.sort((a, b) => a.title < b.title ? 1 : -1);
+        break;
+      }
+
+      case 'sort-price-asc': {
+        data.sort((a, b) => Number(a.price.substring(1)) < Number(b.price.substring(1)) ? -1 : 1);
+        break;
+      }
+
+      case 'sort-price-des': {
+        data.sort((a, b) => Number(a.price.substring(1)) < Number(b.price.substring(1)) ? 1 : -1);
+        break;
+      }
+    }
+
+    return data;
+  }
+
+  // filter the grid
+  filterGrid(data, query) {
+    if (query !== '') {
+      data = data.filter(r => {
+        return (new RegExp(query.toLowerCase())).test(r.title.toLowerCase()) && r;
       });
     }
 
-    this.updateGrid(data);
+    return data;
+  }
+
+  handleKeyup(e) {
+    // updates the state
+    this.state.filterBy = e.target.value;
+
+    this.updateGrid();
+  }
+
+  handleClick(e) {
+    // updates the state
+    this.state.sortBy = e.target.getAttribute('data-action');
+
+    this.updateGrid();
   }
 }
 
