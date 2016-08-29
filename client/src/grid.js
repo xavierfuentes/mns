3bf72b68922b6fea017e5e554a709aa74e499d02
Xavi <xavifuefer@gmail.com>
2016-08-29 20:03:13 +0100

    Add filter feature

--- client/src/grid.js
+++ client/src/grid.js
@@ -3,6 +3,7 @@ import './grid.scss';
 class Grid {
   constructor(data) {
     this.$el = document.getElementById('grid');
+    this.data = data;
     // todo: use localstorage to persist the state
     this.state = {
       data: data,
@@ -13,34 +14,81 @@ class Grid {
 
   render() {
     const frag = document.createDocumentFragment();
+    const grid = document.createElement('section');
+    const toolbar = this.buildToolbar();
 
-    this.state.data.forEach(row => {
-      frag.appendChild(this.buildRow(row));
-    });
+    grid.classList.add('grid-data');
 
+    // toolbar
+    frag.appendChild(toolbar);
+
+    // rows
+    frag.appendChild(grid);
+
+    // render everything
     this.$el.appendChild(frag);
+
+    // update the data inside the grid
+    this.updateGrid(this.state.data);
+  }
+
+  buildToolbar() {
+    const $toolbar = document.createElement('section');
+    const $filter = document.createElement('input');
+    const $label = document.createElement('label');
+
+    $toolbar.classList.add('toolbar');
+
+    $label.innerText = 'filter by title:';
+    $label.appendChild($filter);
+    $filter.addEventListener('keyup', this.handleKeyup.bind(this));
+
+    $toolbar.appendChild($label);
+
+    return $toolbar;
   }
 
   buildRow(rowData) {
-    const row = document.createElement('section');
+    const $row = document.createElement('section');
 
     // add a row class
-    row.classList.add('row');
+    $row.classList.add('row');
 
     // add a class for promotion rows
     if (rowData.hasOwnProperty('promotion') && rowData.promotion === true) {
-      row.classList.add('promotion');
+      $row.classList.add('promotion');
     }
 
-    // todo: we could add events to every row/cell here
-
-    row.innerHTML = `
+    $row.innerHTML = `
     <article class="cell title">${rowData.title || ''}</article>
     <article class="cell price">${rowData.price || ''}</article>
     <article class="cell stars">${rowData.stars || 0}</article>
     `;
 
-    return row;
+    return $row;
+  }
+
+  updateGrid(data) {
+    const grid = this.$el.querySelector('.grid-data');
+
+    grid.innerHTML = '';
+
+    data.forEach(row => {
+      grid.appendChild(this.buildRow(row));
+    });
+  }
+
+  handleKeyup(e) {
+    let data;
+
+    if (e.target.val === '') data = this.data;
+    else {
+      data = this.state.data.filter(r => {
+        return (new RegExp(e.target.value.toLowerCase())).test(r.title.toLowerCase()) && r;
+      });
+    }
+
+    this.updateGrid(data);
   }
 }
 
